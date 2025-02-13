2dafbe18db2f7d5c6e12af0684724dbed3511949
Dong Liang <liangdong2046@gmail.com>
2025-02-13 13:43:35 -0800

    add new models

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,5 +1,8 @@
 import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
+import { anthropic } from "@ai-sdk/anthropic";
+import { google } from '@ai-sdk/google';
+import { deepseek } from '@ai-sdk/deepseek';
+//import { fireworks } from '@ai-sdk/fireworks';
 import {
   customProvider,
   extractReasoningMiddleware,
@@ -11,16 +14,19 @@ export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
 export const myProvider = customProvider({
   languageModels: {
     'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
-    }),
-    'title-model': openai('gpt-4-turbo'),
+    'chat-model-flash': google('gemini-2.0-flash-exp'),
+    'chat-model-large': anthropic('claude-3-5-sonnet-latest'),
+    'chat-model-reasoning': openai('o1-mini'),
+    'chat-model-deepseek': deepseek('deepseek-reasoner'),
+    //'chat-model-reasoning': wrapLanguageModel({
+    //  model: fireworks('accounts/fireworks/models/deepseek-r1'),
+    //  middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    //}),
+    'title-model': openai('gpt-4o-mini'),
     'artifact-model': openai('gpt-4o-mini'),
   },
   imageModels: {
-    'small-model': openai.image('dall-e-2'),
+    'small-model': openai.image('dall-e-3'),
     'large-model': openai.image('dall-e-3'),
   },
 });
@@ -34,17 +40,27 @@ interface ChatModel {
 export const chatModels: Array<ChatModel> = [
   {
     id: 'chat-model-small',
-    name: 'Small model',
-    description: 'Small model for fast, lightweight tasks',
+    name: 'GPT 4o-mini',
+    description: 'Fast and furious, for quick chats',
+  },
+  {
+    id: 'chat-model-flash',
+    name: 'Gemini Flash 2.0',
+    description: 'Multimodal for everyday tasks',
   },
   {
     id: 'chat-model-large',
-    name: 'Large model',
-    description: 'Large model for complex, multi-step tasks',
+    name: 'Claude Sonnet 3.5',
+    description: 'Excellent model for writing stuff',
   },
   {
     id: 'chat-model-reasoning',
-    name: 'Reasoning model',
-    description: 'Uses advanced reasoning',
+    name: 'GPT o1-mini',
+    description: 'When advanced reasoning is needed...',
   },
+  {
+    id: 'chat-model-deepseek',
+    name: "DeepSeek R1",
+    description: "Experience Chinese reasonining..."
+  }
 ];
